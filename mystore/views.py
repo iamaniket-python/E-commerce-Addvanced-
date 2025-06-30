62e2524f88d66895b64cdfeb22d048b3af575656
School <aniketsrivastava57@gmail.com>
2025-06-30 15:16:25 +0530

    first commit

--- mystore/views.py
+++ mystore/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from . models import Product
 # Create your views here.
 def store(request):
+    products = Product.objects.all().filter(is_available=True)
 
-    return render(request, 'store/store.html')
+    context ={
+        'products':products
+    }
+    return render(request, 'store/store.html',context)
--- product/views.py
+++ product/views.py
@@ -1,12 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from commerce.product.models import Category
 from mystore.models import Product
+from django.shortcuts import get_object_or_404
+def home(request,category_slug=None):
+    categories =None
+    products =None
 
-def home(request):
+
+    if category_slug != None:
+        categories = get_object_or_404(Category,category_slug=slug)
     products =Product.objects.all().filter(is_available=True)
-
+    product_count = products.count()
     context ={
-        'products':products
+        'products':products,
+        'product_count':product_count,
     }
     return render(request, 'home.html',context)
 
