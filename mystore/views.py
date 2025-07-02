f12ea0785648463af0a3cbd9d0c06c296bd9f63c
School <aniketsrivastava57@gmail.com>
2025-07-02 12:17:52 +0530

    first commit

--- mystore/views.py
+++ mystore/views.py
@@ -8,3 +8,10 @@ def store(request):
         'products':products
     }
     return render(request, 'store/store.html',context)
+
+
+def product_detail(request,category_slug,product_slug):
+      
+
+  return render(request,'store/product_detail.html')
+
--- product/views.py
+++ product/views.py
@@ -9,8 +9,8 @@ def home(request,category_slug=None):
 
 
     if category_slug != None:
-        categories = get_object_or_404(Category, category_slug=category_slug)
-        products=Product.objects.filter(category=categories,is_available=True)
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=categories, is_available=True)
         product_count = products.count()
     else:
 
