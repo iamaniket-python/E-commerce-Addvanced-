0a9b774320722e6535d8ff43d279bf8add4fec92
School <aniketsrivastava57@gmail.com>
2025-07-02 21:38:31 +0530

    first commit

--- cart/views.py
+++ cart/views.py
@@ -1,38 +1,32 @@
 from django.shortcuts import redirect, render
-from.models import *
 from mystore.models import Product
-
-# Create your views here.
+from .models import Cart, CartItem  # ✅ make sure CartItem is imported
 
 def _cart_id(request):
-    cart= request.session.session_key
+    cart = request.session.session_key
     if not cart:
-        cart=request.session.create()
+        cart = request.session.create()
     return cart
 
-def add_cart(request,product_id):
-    product =Product.objects.get(id=product_id)
+def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    
     try:
-        cart=Cart.objects.get(cart_id=_cart_id(request))
+        cart = Cart.objects.get(cart_id=_cart_id(request))
     except Cart.DoesNotExist:
-        cart=Cart.objects.create(
-            cart_id=_cart_id(request)
-        )
+        cart = Cart.objects.create(cart_id=_cart_id(request))
         cart.save()
+
     try:
-        cart_item =Cart.items.objects.get(product=product,cart=cart)
-        cart_item.quantity +=1
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1
         cart_item.save()
-    except cart_item.DoesNotExist:
-        cart_item=cart_item.objects.create(
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
             product=product,
             quantity=1,
             cart=cart
         )
         cart_item.save()
-    return redirect('cart')
 
-
-def cart(request):
-
-    return render(request, 'store/cart.html')
+    return redirect('cart')  # ✅ make sure 'cart' is the correct name in urls.py
--- staticfiles/assets/js/main.js
+++ staticfiles/assets/js/main.js
@@ -51,3 +51,4 @@ rangeInput.forEach(input =>{
         }
     });
 });
+
