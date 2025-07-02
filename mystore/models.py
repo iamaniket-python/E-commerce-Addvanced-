c76615ba29412df6c7251b3af41b7e4494f12869
School <aniketsrivastava57@gmail.com>
2025-07-02 19:50:58 +0530

    first commit

--- mystore/models.py
+++ mystore/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from product.models import Category
 # Create your models here.
 
@@ -13,6 +14,9 @@ class Product(models.Model):
     category=models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
+
+    def get_url(self):
+        return reverse('product_detail',args=[self.category.slug,self.slug])
 
     def __str__(self):
         return self.pro_name
--- mystore/views.py
+++ mystore/views.py
@@ -1,17 +1,31 @@
-from django.shortcuts import render
-from . models import Product
-# Create your views here.
-def store(request):
-    products = Product.objects.all().filter(is_available=True)
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category  # adjust based on your models
+
+def store(request, category_slug=None):
+    category = None
+    products = Product.objects.all()
+
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+
+    context = {
+        'products': products,
+        'category': category,
+    }
+    return render(request, 'store/store.html', context)
+
+
+def product_detail(request, category_slug, product_slug):
+    
+    try:
+        singel_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+    
 
     context ={
-        'products':products
+        'singel_product':singel_product
     }
-    return render(request, 'store/store.html',context)
 
-
-def product_detail(request,category_slug,product_slug):
-      
-
-  return render(request,'store/product_detail.html')
-
+    return render(request, 'store/product_detail.html', context)
