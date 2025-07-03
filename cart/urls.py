3802adc9cea4912ae82251d7b6492d7068ec6c14
School <aniketsrivastava57@gmail.com>
2025-07-03 19:17:24 +0530

    first commit

--- cart/urls.py
+++ cart/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add_cart/<int:product_id>', views.add_cart, name='add_cart'),
+    path('add_cart/<int:product_id>/', views.add_cart, name='add_cart'),
     path('', views.Cart, name='cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
+from product.models import Category
 from mystore.models import Product
 from .models import Cart, CartItem  # ✅ make sure CartItem is imported
+
+def product_detail(request, category_slug, product_slug):
+    category = get_object_or_404(Category, slug=category_slug)
+    product = get_object_or_404(Product, category=category, slug=product_slug)
+
+    context = {
+        'single_product': product  
+    }
+    return render(request, 'store/product_detail.html', context)
 
 def _cart_id(request):
     cart = request.session.session_key
@@ -29,4 +39,21 @@ def add_cart(request, product_id):
         )
         cart_item.save()
 
-    return redirect('cart')  # ✅ make sure 'cart' is the correct name in urls.py
+    return redirect('cart')  
+
+def cart(request,total=0,quantity=0,cart_items=None):
+    try:
+        cart=Cart.objects.get(cart_id=_cart_id(request))
+        cart_items=CartItem.objects.filter(cart=cart,is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+    except objectNotExist:
+        pass
+
+    context={
+        'total':total,
+        'quantity':quantity,
+        'cart_items':cart_items
+    }
+    return render(request,'cart.html',context)
